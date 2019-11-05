1401b45e07da5b10e61624d709f210fbd23ed41e
Ignacio Bejarano <ignbejarano@gmail.com>
2019-11-05 19:50:05 -0300

    improve app ui

--- src/components/ToggleButtons.js
+++ src/components/ToggleButtons.js
@@ -5,7 +5,7 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 export default function ToggleButtons({handleGroupButton}) {
   return (
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={12}>
         <ButtonGroup fullWidth aria-label="full width outlined button group">
           <Button onClick={() => handleGroupButton('check')}>Check All</Button>
           <Button onClick={() => handleGroupButton('uncheck')}>Uncheck All</Button>
